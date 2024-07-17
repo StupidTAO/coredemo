97fadbfa08e8ea0f6a4009700f349766c52fc88f
caohaitao <2310852380@qq.com>
2024-07-17 15:17:44 +0800

    add LICENSE

--- framework/middleware/recovery.go
+++ framework/middleware/recovery.go
@@ -1,20 +1,18 @@
 package middleware
 
-import "github.com/gohade/hade/framework"
+import "github.com/gohade/hade/framework/gin"
 
 // recovery机制，将协程中的函数异常进行捕获
-func Recovery() framework.ControllerHandler {
+func Recovery() gin.HandlerFunc {
 	// 使用函数回调
-	return func(c *framework.Context) error {
+	return func(c *gin.Context) {
 		// 核心在增加这个recover机制，捕获c.Next()出现的panic
 		defer func() {
 			if err := recover(); err != nil {
-				c.SetStatus(500).Json(err)
+				c.ISetStatus(500).IJson(err)
 			}
 		}()
 		// 使用next执行具体的业务逻辑
 		c.Next()
-
-		return nil
 	}
 }
