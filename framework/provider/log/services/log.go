20f0bf8e237b28271039ef849c695e1ff43e1596
caohaitao <2310852380@qq.com>
2024-08-02 18:53:29 +0800

    add comment

--- framework/provider/log/services/log.go
+++ framework/provider/log/services/log.go
@@ -11,23 +11,27 @@ import (
 )
 
 type HadeLog struct {
+	// 五个必要参数
 	level      contract.LogLevel
 	formatter  contract.Formatter
 	ctxFielder contract.CtxFielder
-
-	output io.Writer
-	c      framework.Container
+	output     io.Writer
+	c          framework.Container
 }
 
+// IsLevelEnable 判断这个级别是否可以打印
 func (log *HadeLog) IsLevelEnbale(level contract.LogLevel) bool {
 	return level <= log.level
 }
 
+// logf 为打印日志的核心函数
 func (log *HadeLog) logf(level contract.LogLevel, ctx context.Context, msg string, fields map[string]interface{}) error {
+	// 先判断日志级别
 	if !log.IsLevelEnbale(level) {
 		return nil
 	}
 
+	// 使用ctxFielder 获取context中的信息
 	fs := fields
 	if log.ctxFielder != nil {
 		t := log.ctxFielder(ctx)
@@ -37,6 +41,20 @@ func (log *HadeLog) logf(level contract.LogLevel, ctx context.Context, msg strin
 			}
 		}
 	}
+
+	// 如果绑定了trace服务，获取trace信息
+	if log.c.IsBind(contract.TraceKey) {
+		tracer := log.c.MustMake(contract.TraceKey).(contract.Trace)
+		tc := tracer.GetTrace(ctx)
+		if tc != nil {
+			maps := tracer.ToMap(tc)
+			for k, v := range maps {
+				fs[k] = v
+			}
+		}
+	}
+
+	// 将日志信息按照formatter序列化为字符串
 	if log.formatter == nil {
 		log.formatter = formatter.TextFormatter
 	}
@@ -45,20 +63,24 @@ func (log *HadeLog) logf(level contract.LogLevel, ctx context.Context, msg strin
 		return err
 	}
 
+	// 如果是panic级别，则使用log进行panic
 	if level == contract.PanicLevel {
 		pkgLog.Panicln(string(ct))
 		return nil
 	}
 
+	// 通过output进行输出
 	log.output.Write(ct)
 	log.output.Write([]byte("\r\n"))
 	return nil
 }
 
+// SetOutput 设置output
 func (log *HadeLog) SetOutput(output io.Writer) {
 	log.output = output
 }
 
+// Panic 输出panic的日志信息
 func (log *HadeLog) Panic(ctx context.Context, msg string, fields map[string]interface{}) {
 	log.logf(contract.PanicLevel, ctx, msg, fields)
 }
@@ -75,6 +97,7 @@ func (log *HadeLog) Warn(ctx context.Context, msg string, fields map[string]inte
 	log.logf(contract.WarnLevel, ctx, msg, fields)
 }
 
+// Info 会打印出普通的日志信息
 func (log *HadeLog) Info(ctx context.Context, msg string, fields map[string]interface{}) {
 	log.logf(contract.InfoLevel, ctx, msg, fields)
 }
--- framework/provider/log/services/rotate.go
+++ framework/provider/log/services/rotate.go
@@ -12,14 +12,19 @@ import (
 	"time"
 )
 
+// HadeRotateLog 代表会进行切割的日志文件存储
 type HadeRotateLog struct {
 	HadeLog
 
+	// 日志文件存储目录
 	folder string
-	file   string
+	// 日志文件名
+	file string
 }
 
+// NewHadeRotateLog 实例化HadeRotateLog
 func NewHadeRotateLog(params ...interface{}) (interface{}, error) {
+	// 参数解析
 	level, ok := params[0].(contract.LogLevel)
 	if !ok {
 		return nil, errors.New("param contrv LogLevel failed")
@@ -46,19 +51,23 @@ func NewHadeRotateLog(params ...interface{}) (interface{}, error) {
 		return nil, errors.New("param contrv appService failed")
 	}
 
+	// 从配置文件中获取folder信息，否则使用默认的LogFolder文件夹
 	folder := appService.LogFolder()
 	if configService.IsExist("log.folder") {
 		folder = configService.GetString("log.folder")
 	}
+	// 如果folder不存在，则创建
 	if !util.Exists(folder) {
 		os.MkdirAll(folder, os.ModePerm)
 	}
 
+	// 从配置文件中获取file信息，否则使用默认的hade.log
 	file := "hade.log"
 	if configService.IsExist("log.file") {
 		file = configService.GetString("log.file")
 	}
 
+	// 从配置文件获取date_format信息
 	dateFormat := "%Y%m%d%H"
 	if configService.IsExist("log.date_format") {
 		dateFormat = configService.GetString("log.date_format")
@@ -67,22 +76,26 @@ func NewHadeRotateLog(params ...interface{}) (interface{}, error) {
 	linkName := rotatelogs.WithLinkName(filepath.Join(folder, file))
 	options := []rotatelogs.Option{linkName}
 
+	// 从配置文件获取rotate_count信息
 	if configService.IsExist("log.rotate_count") {
 		rotateCount := configService.GetInt("log.rotate_count")
 		options = append(options, rotatelogs.WithRotationCount(uint(rotateCount)))
 	}
 
+	// 从配置文件获取rotate_size信息
 	if configService.IsExist("log.rotate_size") {
 		rotateSize := configService.GetInt("log.roate_size")
 		options = append(options, rotatelogs.WithRotationSize(int64(rotateSize)))
 	}
 
+	// 从配置文件获取max_age信息
 	if configService.IsExist("log.max_age") {
 		if maxAgeParse, err := time.ParseDuration(configService.GetString("log.max_age")); err == nil {
 			options = append(options, rotatelogs.WithMaxAge(maxAgeParse))
 		}
 	}
 
+	// 从配置文件获取rotate_time信息
 	if configService.IsExist("log.rotate_time") {
 		if rotateTimeParse, err := time.ParseDuration(configService.GetString("log.rotate_time")); err == nil {
 			options = append(options, rotatelogs.WithRotationTime(rotateTimeParse))
